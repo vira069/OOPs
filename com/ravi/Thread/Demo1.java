ad5b1e2157b2e3e0766296031cae14aceb661679
vira069 <vira.kumar069@gmail.com>
2024-04-24 01:12:33 +0530

    Race condition between threads example

--- com/ravi/Thread/Demo1.java
+++ com/ravi/Thread/Demo1.java
@@ -1,35 +1,59 @@
 package com.ravi.Thread;
 
-class R implements Runnable{
-    @Override
-    public void run() {
-        for(int i=0;i<5;i++) {
-            System.out.println("Hey...!!!");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-}
-class K implements Runnable{
-    @Override
-    public void run() {
-        for(int i=0;i<5;i++) {
+//class R implements Runnable{
+//    @Override
+//    public void run() {
+//        for(int i=0;i<5;i++) {
+//            System.out.println("Hey...!!!");
+//            try {
+//                Thread.sleep(10);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//}
+//class K implements Runnable{
+//    @Override
+//    public void run() {
+//        for(int i=0;i<5;i++) {
+//            System.out.println("Vira...!!!");
+//            try {
+//                Thread.sleep(10);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//}
+public class Demo1 {
+    public static void main(String[] args) {
+        Runnable obj1 = new Runnable() {
+            @Override
+            public void run() {
+                for(int i=0;i<5;i++) {
             System.out.println("Vira...!!!");
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+               e.printStackTrace();
             }
-        }
-    }
-}
-public class Demo1 {
-    public static void main(String[] args) {
-        Runnable obj1 = new R();
-        Runnable obj2 = new K();
+            }
+            }
+        };
+        Runnable obj2 =new Runnable() {
+            @Override
+            public void run() {
+                for(int i=0;i<5;i++) {
+                    System.out.println("Hello");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
 
         Thread thread1 = new Thread(obj1);
         Thread thread2 = new Thread(obj2);
---
+++ com/ravi/Thread/RaceCondition.java
@@ -0,0 +1,40 @@
+package com.ravi.Thread;
+
+
+class Counter{
+    int count = 0;
+    public synchronized void increment(){
+        count++;
+    }
+}
+
+public class RaceCondition {
+    public static void main(String[] args) throws InterruptedException {
+        Counter c = new Counter();
+
+        Runnable obj1 = ()->
+        {
+            for(int i=1;i<=1000;i++)
+            {
+                c.increment();
+            }
+        };
+
+        Runnable obj2 = ()->{
+          for(int i=1;i<=1000;i++)
+          {
+            c.increment();
+          }
+        };
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start();
+
+        t1.join(); // For join (add) the values of Thread 1 and 2
+        t2.join();
+
+        System.out.println(c.count);
+    }
+}
